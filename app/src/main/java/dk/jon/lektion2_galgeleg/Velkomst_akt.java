af4e99b22a1a20121b5a7eb86ecda2e79c5cc053
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-11-02 09:23:04 +0100

    Første commit

---
+++ app/src/main/java/dk/jon/lektion2_galgeleg/Velkomst_akt.java
@@ -0,0 +1,19 @@
+package dk.jon.lektion2_galgeleg;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Velkomst_akt extends Activity implements Runnable{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_velkomst_akt);
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
