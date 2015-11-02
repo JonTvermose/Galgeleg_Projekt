6e625ef495a8e7832502f8fdfcb4da07053a9e48
Jon Tvermose Nielsen <jon.tvermose.nielsen@gmail.com>
2015-11-02 10:46:41 +0100

    Startside lavet - med "animation" vha. Runnable og en Handler.

--- app/src/main/java/dk/jon/lektion2_galgeleg/Velkomst_akt.java
+++ app/src/main/java/dk/jon/lektion2_galgeleg/Velkomst_akt.java
@@ -1,19 +1,81 @@
 package dk.jon.lektion2_galgeleg;
 
 import android.app.Activity;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class Velkomst_akt extends Activity implements Runnable{
+public class Velkomst_akt extends Activity implements View.OnClickListener, Runnable{
+
+    private Galgelogik gl;
+    private Button start;
+    private ImageView galgeImg;
+    private Handler handler = new Handler();
+    private int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_velkomst_akt);
+
+        if(savedInstanceState == null){
+            gl = new Galgelogik();
+            start = (Button) findViewById(R.id.startButton);
+
+            galgeImg = (ImageView) findViewById(R.id.galgeImageView);
+            count = -1;
+        }
+
+        startRunning(500);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        if (v==start){
+
+        }
     }
 
     @Override
     public void run() {
+        switch (count){
+            case -1:
+                galgeImg.setImageResource(R.drawable.galge);
+                break;
+            case 0:
+                galgeImg.setImageResource(R.drawable.forkert1);
+                break;
+            case 1:
+                galgeImg.setImageResource(R.drawable.forkert2);
+                break;
+            case 2:
+                galgeImg.setImageResource(R.drawable.forkert3);
+                break;
+            case 3:
+                galgeImg.setImageResource(R.drawable.forkert4);
+                break;
+            case 4:
+                galgeImg.setImageResource(R.drawable.forkert5);
+                break;
+            case 5:
+                galgeImg.setImageResource(R.drawable.forkert6);
+                count = -2; // reset animation
+                break;
+        }
+        count++;
+        startRunning(500);
+    }
 
+    // Kører run efter "ms" milisekunder
+    public void startRunning(int ms){
+        handler.postDelayed(this, ms);
     }
 }
---
+++ app/src/main/java/dk/jon/lektion2_galgeleg/side2_akt.java
@@ -0,0 +1,13 @@
+package dk.jon.lektion2_galgeleg;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class side2_akt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_side2_akt);
+    }
+}
