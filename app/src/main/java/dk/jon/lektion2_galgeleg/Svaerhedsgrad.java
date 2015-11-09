28f6bc547b68161b3b5b2725dd094b9ea80eebf3
Jacobjepsen27 <Jacob.worck@gmail.com>
2015-11-09 11:08:20 +0100

    Sværhedsgrad klasse oprettet

---
+++ app/src/main/java/dk/jon/lektion2_galgeleg/Svaerhedsgrad.java
@@ -0,0 +1,13 @@
+package dk.jon.lektion2_galgeleg;
+
+import android.app.Activity;
+import android.view.View;
+
+/**
+ * Created by JacobWorckJepsen on 09/11/15.
+ */
+public class Svaerhedsgrad extends Activity {
+
+    
+
+}
